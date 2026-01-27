ec263c7ea299e841d9db521c865520fe3db948ef
MateuszSkoraWsei <mateusz.skora@microsoft.wsei.edu.pl>
2026-01-27 23:02:49 +0100

    Stworzenie Modeli , AppDbContext i całego projektu
    Stworzenie modeli tabel ( ApplicationUser , Transactions, Categories) i konfiguracja pliku AppDbContext Dodatkowo dodanie connection stringa do Program.cs

---
+++ SmartWallet/SmartWallet/Data/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SmartWallet.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Models.ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<Models.Category> Categories { get; set; }
+        public DbSet<Models.Transactions> Transactions { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<Models.Transactions>()
+                                .HasOne(t => t.Sender)
+                .WithMany(u => u.SentTransaction)
+                .HasForeignKey(t => t.SenderId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<Models.Transactions>()
+                .HasOne(t => t.Receiver)
+                .WithMany(u => u.ReceivedTransaction)
+                .HasForeignKey(t => t.ReceiverId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
---
+++ SmartWallet/SmartWallet/Models/Transactions.cs
@@ -0,0 +1,23 @@
+﻿namespace SmartWallet.Models
+{
+    public class Transactions
+    {
+        public int TransactionID { get; set; }
+        public string AccountNumber { get; set; }
+
+        public string TransactionName { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
+
+        public string SenderId { get; set; } = string.Empty;
+        public virtual ApplicationUser Sender { get; set; }
+
+        public string ReceiverId { get; set; } = string.Empty;
+        public virtual ApplicationUser Receiver { get; set; }
+
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+
+    }
+}
